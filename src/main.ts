51d12864b8eaf1798d36281165a9ab8bfaf36218
furiousme <shuptaroks@gmail.com>
2024-11-10 17:39:57 +0200

    docs: write api file in development mode

--- src/main.ts
+++ src/main.ts
@@ -3,6 +3,8 @@ import { AppModule } from './app.module';
 import { ValidationPipe } from '@nestjs/common';
 import { ConfigService } from '@nestjs/config';
 import { SwaggerModule, DocumentBuilder } from '@nestjs/swagger';
+import * as fs from 'node:fs';
+import * as path from 'node:path';
 
 async function bootstrap() {
   const app = await NestFactory.create(AppModule);
@@ -16,6 +18,15 @@ async function bootstrap() {
 
   const documentFactory = () => SwaggerModule.createDocument(app, docConfig);
   SwaggerModule.setup('api', app, documentFactory);
+
+  if (config.get('NODE_ENV') === 'development') {
+    console.log('Write file....');
+    fs.writeFileSync(
+      path.resolve(__dirname, '../doc/api.json'),
+      JSON.stringify(documentFactory(), null, 2),
+      { flag: 'w+' },
+    );
+  }
 
   app.useGlobalPipes(
     new ValidationPipe({
