dab865f19ef5b654f85be26495a87242e0e6a443
furiousme <shuptaroks@gmail.com>
2024-11-18 20:56:26 +0200

    fix: fixes and improvements

--- src/app.module.ts
+++ src/app.module.ts
@@ -20,7 +20,7 @@ import { TypeOrmModule } from '@nestjs/typeorm';
       password: process.env.POSTGRES_PASSWORD,
       database: process.env.POSTGRES_DB,
       autoLoadEntities: true,
-      // synchronize: true,
+      synchronize: true,
     }),
     UserModule,
     ArtistModule,
